64c6dd8c3dc5b47b4fcc73c4f7e2c7b42797269c
Tae One Son <ldstoson@hotmail.com>
2020-02-11 11:16:31 +0900

    seconf commit

---
+++ RunSonarScanner.bat
@@ -0,0 +1,5 @@
+mvn sonar:sonar \
+  -Dsonar.projectKey=helloworld \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=c68577bb96a65753ca3072282927865a30bd690e \
+  -Dsonar.scm.provider=none
